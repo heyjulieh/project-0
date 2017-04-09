117b9b1c6b97c6816694472b0c2959cc2b4e0480
heyjulieh <heyjulieh@gmail.com>
2017-04-08 19:14:40 -0700

    got ghosts to display randomly in grid when start button pressed- yay

--- main.js
+++ main.js
@@ -10,9 +10,11 @@ function onTimer() {
     setTimeout(onTimer, 1000);
   }
 }
+function randomImages (){
   var images = ["ghost_png/001-halloween.png", "ghost_png/002-ghost-2.png", "ghost_png/003-ghost-1.png", "ghost_png/004-ghost.png", "ghost_png/005-game-ghost.png"];
   $('.box').each(function () {
       var rand = Math.floor(Math.random() * 5);
       $(this).append('<img src="' + images[rand] + '"/>');
       return;
   });
+}
